2e96132725c59c194d9d6987d02f27442dcb4295
guizao1712 <guilherme.leonardi.cruz@escola.pr.gov.br>
2026-08-17 11:58:26 -0300

    b

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const caixaPrincipal = document. querySelector('.caixa-principal')
 const caixaPerguntas = document. querySelector('.caixa-perguntas')
 const caixaAlterativa = document. querySelector('.caixa-alternativa')
 const caixaResultado = document. querySelector('.caixa-resultado')
+const listsa = [item1, item2]
+const perguntas = {
+    tamanho; 20,
+    tipo 'HB'
+    cor; 'grafite',
+    temBorrachaAtras; false
+} 
+const perguntas1 =[
+    {
+        enunciado; "Pergunta1",
+        alternativas;[
+            "alternativa1",
+            "alternativa2"
+        ],
+    },
+]
+const perguntas2 =[
+    {
+        enunciado; "Pergunta1",
+        alternativas;[
+            "alternativa1",
+            "alternativa2"
+        ],
+    },
+]
