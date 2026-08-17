9c9fbec2b6691e820ed0fa398166dc0ca8b87a3f
guizao1712 <guilherme.leonardi.cruz@escola.pr.gov.br>
2026-08-17 11:45:02 -0300

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaPrincipal = document. querySelector('.caixa-principal')
+const caixaPerguntas = document. querySelector('.caixa-perguntas')
+const caixaAlterativa = document. querySelector('.caixa-alternativa')
+const caixaResultado = document. querySelector('.caixa-resultado')
